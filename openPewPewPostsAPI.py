b843a7ca7e288a661cde68f101fbaebfafc0e739
Yannick BERCKMANS <13069@ecam.be>
2018-04-24 14:40:06 +0200

    Adding self routes, posts by ID, posts by user

--- openPewPewPostsAPI.py
+++ openPewPewPostsAPI.py
@@ -1,7 +1,10 @@
 #!flask/bin/python
 from flask import Flask, jsonify, make_response, request
 from flask import request
+
 import sqlite3
+import time
+import jwt
 
 app = Flask(__name__)
 
@@ -34,7 +37,9 @@ def createPostsTable():
 # Create a post in the posts table
 def addPostToDb(userId, content, timestamp):
 	[conn, c] = connectDb()
-	c.execute("INSERT INTO posts VALUES(?,?,?)",(int(userId),str(content),int(timestamp)))
+	
+	# Specify the column necessary because there is an ID
+	c.execute("INSERT INTO posts(user,content,date) VALUES(?,?,?)",(int(userId),str(content),int(timestamp)))
 	if(closeDb(conn, c)==True):
 		print("Correctly closed the DB")
 	else:
@@ -42,17 +47,27 @@ def addPostToDb(userId, content, timestamp):
 	return True
 
 # Get all posts from the posts table
-def getPostsFromDb():
+# If a userId is defined, return all posts of this user
+
+def getPostsFromDb(userId=0):
 	[conn, c] = connectDb()
-	posts = c.execute("select * from posts")
 	postList=[]
+	
+	if(userId == 0): 
+		posts = c.execute("select * from posts")
+	else:
+		posts = c.execute("select * from posts WHERE posts.user IS ?",(userId,))
+		
+	
 	jsonResponse = {'payload':[]}
 	
 	for post in posts:
+		print(post)
 		postInfo = {
-			'userId':post[0],
-			'content':post[1],
-			'timestamp':post[2],
+			'id':post[0],
+			'userId':post[1],
+			'content':post[2],
+			'timestamp':post[3],
 		}
 		postList.append(postInfo)
 	
@@ -60,6 +75,41 @@ def getPostsFromDb():
 		jsonResponse['payload'].append(element)
 	
 	closeDb(conn,c)
+	return jsonResponse
+	
+def getPostFromDb(userId=None, postId=None):
+	[conn, c] = connectDb()
+	postList=[]
+	jsonResponse = {'payload':[]}
+	
+	# If both of them are defined, we send an error
+	
+	if(userId is not None and postId is "last"):
+		posts = c.execute("select * from posts WHERE posts.user IS ? ORDER BY date DESC LIMIT 1",(int(userId),))
+	elif(userId is None and postId is not None):
+		posts = c.execute("select * from posts WHERE posts.id IS ?",(int(postId),))
+	elif(userId is None and postId is None):
+		return False	
+	elif(userId is not None and postId is None):
+		return False
+	else:
+		posts=[]
+	
+	for post in posts:
+		# posts[0] is id, 1 is userId, 2 is content and 3 is timestamp
+		postInfo = {
+			'id': post[0],
+			'userId':post[1],
+			'content':post[2],
+			'timestamp':post[3],
+		}
+		postList.append(postInfo)
+	
+	for element in postList:
+		jsonResponse['payload'].append(element)
+	
+	closeDb(conn,c)
+	
 	return jsonResponse
 		
 # ----------------------------------------------------------------
@@ -70,10 +120,35 @@ def getPostsFromDb():
 def getPosts():
 	jsonResponse = getPostsFromDb()
 	return jsonify(jsonResponse),200
+	
+@app.route('/posts/<int:postId>',methods=['GET'])
+def getPost(postId):
+	jsonResponse = getPostFromDb(postId=postId)
+	return jsonify(jsonResponse),200
+	
+@app.route('/users/<int:userId>/posts',methods=['GET'])
+def getUserPosts(userId):
+	jsonResponse = getPostsFromDb(userId)
+	return jsonify(jsonResponse),200
 
-@app.route('/posts/<int:post_id>',methods=['GET'])
-def getPost(post_id):
-	return 'Return the post: '+str(post_id)	
+@app.route('/users/<int:userId>/posts/last',methods=['GET'])
+def getlastPost(userId):
+	jsonResponse = getPostFromDb(userId=userId, postId="last")
+	return jsonify(jsonResponse),200
+	
+@app.route('/user/posts',methods=['GET'])
+def getMyPosts():
+	# This need to be changed with the TOKEN
+	myId = "13069"
+	jsonResponse = getPostsFromDb(myId)
+	return jsonify(jsonResponse),200
+
+@app.route('/user/posts/last',methods=['GET'])
+def getMyLastPost():
+	# This need to be changed with the TOKEN
+	myId = "13069"
+	jsonResponse = getPostFromDb(userId=myId, postId="last")
+	return jsonify(jsonResponse),200
 
 # ----------------------------------------------------------------
 # POST
@@ -82,16 +157,20 @@ def getPost(post_id):
 # Creating a new POST
 @app.route('/posts',methods=['POST'])
 def postPost():
+	
+	# We don't expect timestamp, we'll generate it
+	# We don't need the user, it is in the token
+	
+	# This need to be changed with the TOKEN
+	myId = "13069"
 	if (not request.json or
-	not 'userId' in request.json['payload'] or
-	not 'content' in request.json['payload'] or
-	not 'timestamp' in request.json['payload']):
+	not 'content' in request.json['payload']):
 		jsonResponse = {
 			'payload': 'Wrong payload',
 		}
 		return jsonify(jsonResponse),400
 	else:
-		check = addPostToDb(request.json['payload']['userId'],request.json['payload']['content'],request.json['payload']['timestamp'])
+		check = addPostToDb(int(myId),request.json['payload']['content'],int(time.time()))
 		if(check == True):
 			jsonResponse = {
 				'payload': 'Correctly created the post',
@@ -104,7 +183,14 @@ def postPost():
 			return jsonify(jsonResponse), 500
 	return 404
 
+# ----------------------------------------------------------------
+# TOKEN GESTURE
+# ----------------------------------------------------------------
+def tokenToId(token):
+	secretKey = "secret"
+	
 # Main script
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(debug=True, host='127.0.0.1')
+	# app.run(debug=True)
 	closeDb(conn,c)
